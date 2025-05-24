11ad8e001536a71e07a47183c1f6c2a6874cbc1e
Анна Селицкая <annaselitskaya@MacBook-Air-Anna-2.local>
2025-05-24 19:44:39 +0200

    Zmiany

--- kubernetes/applay.sh
+++ kubernetes/applay.sh
@@ -2,7 +2,7 @@
 
 # Wdrożenia
 # Metrics Server
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl apply -f  metrics_server/
 
 # Prometheus
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
@@ -23,23 +23,16 @@ kubectl apply -f redis/
 echo "Czekam na gotowość podów..."
 
 # Czekaj na gotowość podów backend i frontend (przykład)
-kubectl wait --for=condition=available deployment/backend --timeout=120s
-kubectl wait --for=condition=available deployment/frontend --timeout=120s
-kubectl wait --for=condition=available deployment/grafana -n monitoring --timeout=120s
-kubectl wait --for=condition=available deployment/keycloak --timeout=120s
-kubectl wait --for=condition=available deployment/postgres --timeout=120s
-kubectl wait --for=condition=available deployment/prometheus -n monitoring --timeout=120s
-kubectl wait --for=condition=available deployment/redis --timeout=120s
-
+sleep 30
 echo "Wszystkie deploymenty gotowe, uruchamiam port-forward..."
 
-# Uruchom port-forward w tle (lub otwórz nowe terminale)
+# Uruchom port-forward w tle 
 kubectl port-forward svc/backend 5001:5001 &
 kubectl port-forward svc/frontend 3001:3001 &
-kubectl port-forward svc/grafana 3000:80 -n monitoring &
+kubectl port-forward svc/prometheus-operator-grafana 3002:80 -n monitoring &
 kubectl port-forward svc/keycloak 8080:8080 &
 kubectl port-forward svc/postgres 5432:5432 &
-kubectl port-forward svc/prometheus 9090:9090 -n monitoring &
+kubectl port-forward svc/prometheus-operator-kube-p-prometheus 9090:9090 -n monitoring &
 kubectl port-forward svc/redis 6379:6379 &
 
 echo "Port-forwardy uruchomione"
@@ -48,14 +41,16 @@ wait
 
 
 # Poprawność działania 
+
 # Metrics Server
-# export KUBE_EDITOR="nano"
-#   args:
-        # - --cert-dir=/tmp
-        # - --secure-port=4443
-        # - --kubelet-preferred-address-types=InternalIP,Hostname,InternalDNS,ExternalDNS,ExternalIP
-        # - --kubelet-insecure-tls   
 # kubectl get deployment metrics-server -n kube-system
 
 # HPA
 # kubectl get hpa
+
+
+# Helm
+# helm install myapp ./mychart (LUB POKAZAC ZRZUT)
+# helm list
+# helm status myapp
+
